5786e6ba14236920880d9dc0db74b975a5b2814c
Mh-NOUHICoder <mohammednouhi61@gmail.com>
2025-09-19 23:38:44 +0100

    3. Rendering Components

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,13 @@
+interface PillProps {
+  title: string;
+}
+
+const Pill: React.FC<PillProps> = ({ title }) => {
+  return (
+    <div className="flex justify-center items-center bg-[blue] px-3 w-auto h-[27px] rounded-full">
+      <p className="text-sm text">{title}</p>
+    </div>
+  );
+};
+
+export default Pill;
