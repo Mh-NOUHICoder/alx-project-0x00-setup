cc0798a2769f3e11ac100f7f238803efbef8f584
Mh-NOUHICoder <mohammednouhi61@gmail.com>
2025-09-20 17:12:29 +0100

    fix the rounded-full missing

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -7,6 +7,6 @@ export interface ButtonProps {
   title: string;
   size?: "small" | "medium" | "large";
   
-  shape?: "rounded-sm" | "rounded-md" | "rounded-lg";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
   styles: string; 
 }
